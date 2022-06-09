3c8b1527cd9f58e1a875b52762969df9ef32b4d8
hirarira <hirarira617@gmail.com>
2022-06-09 23:48:49 +0900

    ゲームのBaseObjectを追加

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,28 @@
 "use strict";
-(()=>{
-  console.log("test");
-})()
+
+class GameObject {
+  canvas: HTMLElement;
+  ctx: any;
+  /** 画面の解像度 */
+  windowSize: number;
+
+  constructor() {
+    const canvas: any = document.getElementById('canvas');
+    this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');
+    this.windowSize = 1000;
+  }
+
+  draw() {
+    this.ctx.clearRect(0, 0, this.windowSize, this.windowSize);
+    this.ctx.arc( 100, 100, 10, 0 * Math.PI / 180, 360 * Math.PI / 180, false ) ;
+    this.ctx.fillStyle = "rgba(255,0,0,0.8)" ;
+    this.ctx.fill() ;
+  }
+
+}
+
+window.onload = () => {
+  const gameObjet = new GameObject();
+  gameObjet.draw();
+}
